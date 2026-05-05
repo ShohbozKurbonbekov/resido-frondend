caf42261dfe69cc4f9d51d313d8682ff94c92ccd
ShohbozKurbonbekov <skurbonbekov98@gmail.com>
2026-05-06 02:52:52 +0900

    fix(deploy-shell):modify deploy shell

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,4 @@ git reset --hard
 git checkout master
 git  pull origin master
 
-npm i yarn -g
-yarn global add serve
-yarn
-yarn run build
-pm2 serve dist 1004 --spa --name RESIDO-CLIENT
-
-
-#! DEVELOPMENT
-# git reset --hard
-# git checkout develop
-# git pull origin develop
-
-# npm i
-# pm2 start process.config.js --env development
+docker compose up -d
